df4a4ad3132478004d36048f606c04e5ccecdcb6
CarolineBenn <lineybenn@gmail.com>
2016-09-24 00:41:19 +0100

    Can access json if hosted on same server

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 $(function(){
-  console.log("HELLO")
+
+  // Check it works:
+  console.log("HELLO");
+
+  // Make the ajax call:
+  $.ajax({
+      url: 'http://carolinebenn.co.uk/bigchill/bigchill.json',
+      dataType: 'JSON',
+      jsonCallback: 'callback',
+      type: 'GET',
+      success: function(res) {
+        showImages(res.data);
+      }
+  });
+
+  function showImages(data){
+    $(data).each(function(index, item){
+      console.log(item);
+    });
+  }
+
+
+
+
+
+
+
 })
