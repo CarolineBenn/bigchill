261690b6e6256fefd8ef21a0226521d2355a1db8
CarolineBenn <lineybenn@gmail.com>
2016-09-24 01:07:43 +0100

    Displys a new image and caption every two seconds

--- js/app.js
+++ js/app.js
@@ -5,25 +5,31 @@ $(function(){
 
   // Make the ajax call:
   $.ajax({
-      url: 'http://carolinebenn.co.uk/bigchill/bigchill.json',
-      dataType: 'JSON',
-      jsonCallback: 'callback',
-      type: 'GET',
-      success: function(res) {
-        showImages(res.data);
-      }
+    url: 'http://carolinebenn.co.uk/bigchill/bigchill.json',
+    dataType: 'JSON',
+    jsonCallback: 'callback',
+    type: 'GET',
+    success: function(res) {
+      setInterval(function(){
+        bigChill(res.data);
+      }, 2000);
+    }
   });
 
-  function showImages(data){
-    $(data).each(function(index, item){
-      console.log(item);
-    });
+  var h1Tag  = $('#title');
+  var imgTag = $('#main-image');
+
+
+  function bigChill(data){
+    // var randomNumber = data[Math.floor(Math.random() * 10)]
+    var bigChillMoment = data[Math.floor(Math.random() * data.length)];
+    
+    var title = bigChillMoment.title;
+    var image = bigChillMoment.image;
+
+    h1Tag.html(title);
+    imgTag.attr("src", image);      
+
   }
 
-
-
-
-
-
-
-})
+});
